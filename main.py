e9871f33dda5d344ac72046bd4c46918130b5942
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-28 21:34:00 +0200

    Make temperature lineplot with real data

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ option = st.selectbox("Select data to view",
                       options=("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates, temperatures = get_data(place, days, option)
+if place:
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=dates, y=temperatures, 
-                 labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, 
+                        labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+        
+    if option == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
