804249379fa7b127efb68a031e49355e0748d04f
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-28 21:15:09 +0200

    Make function get Temperature or sky condition

--- backend.py
+++ backend.py
@@ -6,13 +6,19 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-def get_data(place, forecast_days=None, view_type=None):
+def get_data(place, forecast_days, view_type):
 
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-
-    return data
+    filtered_data = data["list"]
+    number_of_values = 8 * forecast_days
+    filtered_data = filtered_data[:number_of_values]
+    if view_type == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if view_type == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, view_type="Temperature"))
