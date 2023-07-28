3fb161cd0e365975e1828468c157f87da422ae45
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-28 20:47:35 +0200

    Initial commit

---
+++ backend.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+def get_data(place, forecast_days=None, view_type=None):
+
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    return data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
